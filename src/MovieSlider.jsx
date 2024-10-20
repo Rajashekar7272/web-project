dd8ad8110a39847d999f7b927af9896443a00065
Rajashekar <kanaparthirajashekar123@gmail.com>
2024-10-20 14:51:37 +0530

    code changes and styling

--- src/MovieSlider.jsx
+++ src/MovieSlider.jsx
@@ -9,8 +9,9 @@ const MovieSlider = () => {
         "Salaar",
         "Devara Part 1",
         "Leo",
-        "Vikram",
-        "Kaithi",
+        "The Greatest of All Time",
+        "Game of Thrones",
+        "Breaking Bad",
         "Manjummel Boys",
         "Minnal Murali",
         "K.G.F: Chapter 2",
@@ -69,22 +70,30 @@ const MovieSlider = () => {
         return () => clearInterval(interval); // Clear interval on unmount
     }, [movies.length]);
 
+    // Function to truncate plot
+    const truncatePlot = (plot) => {
+        const maxLength = 250; // Set max length for the plot
+        return plot.length > maxLength ? `${plot.substring(0, maxLength)}...` : plot;
+    };
+
     return (
         isVisible && (
             <div className="relative mx-auto p-5 overflow-hidden">
-                <h2 className="font-bold mb-4 text-white animate-scrolling">Search Your Top Movies and Enjoy Watching Your Favourite Movies and Series And Search Seamlessly Which Gives Best Results</h2>
+                <h2 className="font-semibold mb-4 text-white animate-scrolling">Search Your Top Movies and Enjoy Watching Your Favourite Movies and Series And Search Seamlessly Which Gives Best Results</h2>
                 <div className="flex transition-transform duration-500 ease-in-out"
                      style={{ transform: `translateX(-${currentIndex * 220}px)` }}>
                     {movies.map((movie, index) => (
                         <div key={index} className={`flex flex-col items-center justify-center min-w-[200px] m-2 transition-all duration-300 ${index === currentIndex ? 'w-[500px]' : 'w-[200px]'} bg-gradient-to-tl from-black to-purple-800 rounded-lg shadow-2xl`}>
                             {movie.Response === "True" ? (
-                                <div className={`flex ${index === currentIndex ? 'h-80' : 'h-60'}`}>
+                                <div className={`flex ${index === currentIndex ? 'h-80' : 'h-60'} overflow-hidden`}>
                                     <img src={movie.Poster} alt={movie.Title} className={`h-full object-contain rounded-md transition-transform duration-300 ${index === currentIndex ? 'scale-70' : ''}`} />
                                     {index === currentIndex && (
                                         <div className="ml-4 text-left h-full">
                                             <h3 className="font-semibold text-white text-2xl">{movie.Title} ({movie.Year})</h3>
-                                            <p className="text-gray-400">IMDB: {movie.imdbRating}</p>
-                                            <p className="text-white mt-3 font-medium">{movie.Plot}</p>
+                                            <p className="text-gray-400 font-medium">IMDB: {movie.imdbRating}</p>
+                                            <p className='text-gray-400'>Languages: {movie.Language}</p>
+                                            <p className='text-gray-400'>Genre: {movie.Genre}</p>
+                                            <p className="text-white mt-3 font-medium overflow-hidden">{truncatePlot(movie.Plot)}</p>
                                         </div>
                                     )}
                                 </div>
